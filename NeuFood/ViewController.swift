14cf8ac547b9352b21952778df70d4b552ae9601
S0daWh1skey <albertsphepherd@alberts-macbook-pro.local>
2017-09-19 10:23:22 +0800

    Initial Commit

---
+++ NeuFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeuFood
+//
+//  Created by Albert Sphepherd on 2017/9/19.
+//  Copyright © 2017年 Albert Sphepherd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
